c5f9d162c19dd0060828fac6cb3fea9036952751
jcorneli <jaybart@protonmail.com>
2022-04-05 15:11:17 +0200

    small bug

--- src/main/init.c
+++ src/main/init.c
@@ -23,8 +23,6 @@ t_err	rt_mlx_init(t_mlx *mlx)
 
 void	init_temp_scene(t_minirt *rt)
 {
-	t_rt_shape	*node;
-
 	rt->scene.origin = (t_vector){0, 0, 0};
 	rt->scene.shapes = new_sphere(250, (t_vector){0, -100, 800}, (t_color){255, 0, 0, 255});//RED
 	rt->scene.shapes->next = new_sphere(250, (t_vector){200, 0, 900}, (t_color){0, 0, 255, 255});//BLUE
@@ -33,7 +31,7 @@ void	init_temp_scene(t_minirt *rt)
 
 	rt->scene.ambient_ligth = new_ambient_light(1.124, (t_color){255 ,255 ,255 ,255});
 	rt->scene.lights = new_point_light(1, (t_position){-50, -180, 100}, (t_color){255 ,255 ,255 ,255});
-	printf("ambient:%f\n", rt->scene.ambient_ligth.ratio);
+	// printf("ambient:%f\n", rt->scene.ambient_ligth.ratio);
 }
 
 t_err	rt_init(t_minirt *rt)
